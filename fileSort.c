adcd87d7ce4c7ff24fb8e532014b603ece62852a
Toshanraju Vysyaraju <tv135@ilab1.cs.rutgers.edu>
2020-02-19 11:12:39 -0500

    fixed comments

--- fileSort.c
+++ fileSort.c
@@ -2,17 +2,17 @@
 #include<stdlib.h>
 
 // TOSHAN: 
-  //   - LL helper functions (maybe)
-  //   - reading cmd line input
-  //      - checking flags
-  //      - opening files
-  //   - add all tokens into linked list
-  //
-  // REAGAN:
-  //   - sorting
-  //      - quicksort
-  //      - insertion sort
-  //   -  comparators
+//   - LL helper functions (maybe)
+//   - reading cmd line input
+//      - checking flags
+//      - opening files
+//   - add all tokens into linked list
+//
+// REAGAN:
+//   - sorting
+//      - quicksort
+//      - insertion sort
+//   -  comparators
 
 typedef struct Node {
   void* data;
